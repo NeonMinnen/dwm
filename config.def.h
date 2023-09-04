d2eee0f273ad7ae6f8fb20ba9dbdae9868838b08
AutoDys <matt@n3on.io>
2023-09-04 02:29:28 -0700

    Stable before Patching Hell

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -19,7 +19,7 @@ static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, "#000000" },
 	[SchemeSel]  = { col_gray4, col_cyan,  "#ffffff" },
 };
 
